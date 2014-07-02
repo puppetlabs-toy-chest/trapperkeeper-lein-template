43c9fffd6dee3cc18e3a3b116d92faff06c002c5
Preben Ingvaldsen <preben@puppetlabs.com>
2014-07-02 10:47:18 -0700

    (TK-12) Update dependency versions
    Update all of the dependencies in the project.clj file to the
    most recent versions.

--- resources/leiningen/new/trapperkeeper/project.clj
+++ resources/leiningen/new/trapperkeeper/project.clj
@@ -1,22 +1,22 @@
-(def tk-version "0.3.4")
-(def ks-version "0.5.3")
+(def tk-version "0.4.2")
+(def ks-version "0.7.2")
 
 (defproject {{raw-name}} "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.6"]
-                 [org.clojure/tools.logging "0.2.6"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.8"]
+                 [org.clojure/tools.logging "0.3.0"]
                  [puppetlabs/trapperkeeper ~tk-version]]
 
   :test-paths ["test" "test-resources"]
 
-  :profiles {:dev {:dependencies [[puppetlabs/trapperkeeper-webserver-jetty9 "0.3.3"]
+  :profiles {:dev {:dependencies [[puppetlabs/trapperkeeper-webserver-jetty9 "0.5.2"]
                                   [puppetlabs/trapperkeeper ~tk-version :classifier "test" :scope "test"]
                                   [puppetlabs/kitchensink ~ks-version :classifier "test" :scope "test"]
-                                  [clj-http "0.7.9"]
+                                  [clj-http "0.9.2"]
                                   [ring-mock "0.1.5"]]}}
 
   :aliases {"tk" ["trampoline" "run" "--config" "test-resources/config.conf"]}
