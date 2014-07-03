699e221cb49937eac004a2b49f396dd2cf2f2303
Chris Price <chris@puppetlabs.com>
2014-07-03 11:13:42 -0700

    Add `deploy-repositories` to project.clj

--- project.clj
+++ project.clj
@@ -1,2 +1,5 @@
 (defproject trapperkeeper/lein-template "0.1.1"
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :sign-releases false}]])
