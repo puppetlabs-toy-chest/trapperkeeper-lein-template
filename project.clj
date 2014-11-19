5e6a297a603734e71288f42d5c4e0ca5ca5e4839
Chris Price <chris@puppetlabs.com>
2014-11-19 12:25:42 -0800

    (MAINT) Changes to bump up to trapperkeeper 1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject trapperkeeper/lein-template "0.1.3-SNAPSHOT"
+(defproject trapperkeeper/lein-template "1.0.0"
   :eval-in-leiningen true
 
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
